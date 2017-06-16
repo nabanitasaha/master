bb47fa606c40c5f020d56efbadd54eb427a7fcb4
Nabanita Saha <nabanita089@gmail.com>
2017-06-16 14:36:32 -0700

    initial commit

---
+++ ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Nabanita on 5/23/17.
+//  Copyright © 2017 Nabanita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func showMessage() {
+        let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello World", preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+
+}
+
